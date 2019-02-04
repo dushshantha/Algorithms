ad344a5e4e2b40c55ce8c40ac426b08fde71a59e
dushshantha <dushshantha.chandaradasa@gmail.com>
2019-02-04 11:19:13 -0500

    Add files via upload

--- Consecutives.py
+++ Consecutives.py
@@ -1,16 +1,21 @@
+import collections
 
 def getLongestConsecutives(matrix):
 
+    cache = {}
     max_len = 0
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            max_len = max(max_len, getMax(matrix, i , j))
+            max_len = max(max_len, getMax(cache,matrix, i , j))
             print(max_len)
 
     return max_len
 
 
-def getMax(matrix, i, j):
+def getMax(cache, matrix, i, j):
+    if str(i)+','+str(j) in cache:
+        return cache[str(i)+','+str(j)]
+
     length = 1
     print(i, j, matrix[i][j])
 
@@ -21,15 +26,15 @@ def getMax(matrix, i, j):
         return 0
 
     if i > 0 and matrix[i - 1][j] - 1 == matrix[i][j]:
-        length += getMax(matrix, i - 1, j)
+        length += getMax(cache, matrix, i - 1, j)
     if i < h -1 and matrix[i + 1][j] - 1 == matrix[i][j]:
-        length += getMax(matrix, i + 1, j)
+        length += getMax(cache, matrix, i + 1, j)
     if j > 0 and matrix[i][j-1] - 1 == matrix[i][j]:
-        length += getMax(matrix, i, j - 1)
+        length += getMax(cache, matrix, i, j - 1)
     if j < w - 1 and matrix[i][j+1] - 1 == matrix[i][j]:
-        length += getMax(matrix, i, j + 1)
+        length += getMax(cache, matrix, i, j + 1)
 
-
+    cache[str(i)+','+str(j)] = length
     return length
 
 if __name__ == '__main__':
