1f0c2fdc7a21d527f36e9bdc2a2f86083a374186
Marcus Sinhalage <dushshantha.chandaradasa@gmail.com>
2019-03-21 09:44:32 -0400

    Added changes

--- stackwithmin.py
+++ stackwithmin.py
@@ -1,29 +1,42 @@
 class Node:
-    def __init__(self, data, next = None):
+    def __init__(self, data, next = None, min = None):
         self.data = data
         self.next = next
+        self.min = min
 
 class stack:
     def __init__(self, head = None):
         self.head = head
-        self.min = head
-        self.preMin = None
-    
+        self.head.min = head
+
     def push(self, newNode):
-        if newNode.data < self.min.data:
-            self.preMin = self.min
-            self.min = newNode
+        if newNode.data < self.head.min.data:
+            newNode.min = newNode
+        else:
+            newNode.min = self.head.min
 
         newNode.next = self.head
         self.head = newNode
 
     def pop(self):
         ret = self.head
-        if self.head ==  self.min:
-            self.min = self. preMin
         self.head = self.head.next
         ret.next = None
         return ret
 
+    def getmin(self):
+        return self.head.min
 
 
+
+if __name__ == '__main__':
+    s = stack(Node(data = 5))
+    s.push(Node(data = 6))
+    s.push(Node(data = 3))
+    s.push(Node(data = 4))
+
+    print(s.getmin().data == 3)
+    s.pop()
+    print(s.getmin().data == 3)
+    s.pop()
+    print(s.getmin().data == 5)
