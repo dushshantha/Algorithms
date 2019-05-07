bf64272a2558eeecfe951fa93ccde6148b377109
dushshantha <dushshantha.chandaradasa@gmail.com>
2019-05-06 21:42:30 -0400

    added tictactoe

--- TicTacToe.py
+++ TicTacToe.py
@@ -44,17 +44,31 @@ class TTTBoard:
     def ramdomMove(self):
         x = random.randint(0, 2)
         y = random.randint(0, 2)
-        while not self.makeMove(x,y, "O"):
-            x = random.randint(0, 2)
-            y = random.randint(0, 2)
+        if not self.isBoardFull():
+            while not self.makeMove(x,y, "O"):
+                x = random.randint(0, 2)
+                y = random.randint(0, 2)
 
     def isPlayerWon(self):
-        return True
+        return self.verticalTrue('X') or self.horizontalTrue('X') or self.diagonalTrue('X')
+
+    def isComputerWon(self):
+        return self.verticalTrue('O') or self.horizontalTrue('O') or self.diagonalTrue('O')
 
     def getMove(self):
         s = input("Your Move (X,Y) : ")
         return int(s.split(',')[0]), int(s.split(',')[1])
 
+    def verticalTrue(self, token):
+        return self.grid[0][0] == self.grid[1][0] == self.grid[2][0] == token or self.grid[0][1] == self.grid[1][1] == self.grid[2][1] == token or \
+               self.grid[0][2] == self.grid[1][2] == self.grid[2][2] == token
+
+    def horizontalTrue(self, token):
+        return self.grid[0][0] == self.grid[0][1] == self.grid[0][2] == token or self.grid[1][0] == self.grid[1][1] == self.grid[1][2] == token or \
+               self.grid[2][0] == self.grid[2][1] == self.grid[2][2] == token
+
+    def diagonalTrue(self, token):
+        return self.grid[0][0] == self.grid[1][1] == self.grid[2][2] == token or self.grid[2][0] == self.grid[1][1] == self.grid[0][2] == token
 
 
 if __name__ == "__main__" :
@@ -68,8 +82,17 @@ if __name__ == "__main__" :
 
         board.makeMove(x,y, "X")
         board.printBoard()
+        if board.isPlayerWon():
+            print("Congrats!!! YOU WON")
+            break
+
         time.sleep(3)
         print("Computer's Move")
         board.ramdomMove()
         board.printBoard()
         time.sleep(3)
+        if board.isComputerWon():
+            print("Game Over!!! YOU LOSE")
+            break
+
+    print("Game Over!!!")
